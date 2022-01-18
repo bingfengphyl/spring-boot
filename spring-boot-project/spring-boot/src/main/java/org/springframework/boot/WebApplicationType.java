da9b22221785b1d0e48bafbf7d4aa858183bc488
yeff <yeff@fotile.com>
2022-01-18 15:03:08 +0800

    1. 进度 http://svip.iocoder.cn/Spring-Boot/SpringApplication/#2-1-1-getSpringFactoriesInstances

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
@@ -25,69 +25,88 @@ import org.springframework.util.ClassUtils;
  * @author Brian Clozel
  * @since 2.0.0
  */
-public enum WebApplicationType {
+// Web 应用类型
+public enum WebApplicationType
+{
 
-	/**
-	 * The application should not run as a web application and should not start an
-	 * embedded web server.
-	 */
-	NONE,
+    /**
+     * The application should not run as a web application and should not start
+     * an embedded web server.
+     */
+    // 非内嵌的 Web 应用
+    NONE,
 
-	/**
-	 * The application should run as a servlet-based web application and should start an
-	 * embedded servlet web server.
-	 */
-	SERVLET,
+    /**
+     * The application should run as a servlet-based web application and should
+     * start an embedded servlet web server.
+     */
+    // 内嵌的 Servlet Web 应用。例如说，Spring MVC 。
+    SERVLET,
 
-	/**
-	 * The application should run as a reactive web application and should start an
-	 * embedded reactive web server.
-	 */
-	REACTIVE;
+    /**
+     * The application should run as a reactive web application and should start
+     * an embedded reactive web server.
+     */
+    // 内嵌的 Reactive Web 应用。例如说，Spring Webflux 。
+    REACTIVE;
 
-	private static final String[] SERVLET_INDICATOR_CLASSES = { "javax.servlet.Servlet",
-			"org.springframework.web.context.ConfigurableWebApplicationContext" };
+    private static final String[] SERVLET_INDICATOR_CLASSES = { "javax.servlet.Servlet",
+	    "org.springframework.web.context.ConfigurableWebApplicationContext" };
 
-	private static final String WEBMVC_INDICATOR_CLASS = "org.springframework." + "web.servlet.DispatcherServlet";
+    private static final String WEBMVC_INDICATOR_CLASS = "org.springframework." + "web.servlet.DispatcherServlet";
 
-	private static final String WEBFLUX_INDICATOR_CLASS = "org." + "springframework.web.reactive.DispatcherHandler";
+    private static final String WEBFLUX_INDICATOR_CLASS = "org." + "springframework.web.reactive.DispatcherHandler";
 
-	private static final String JERSEY_INDICATOR_CLASS = "org.glassfish.jersey.servlet.ServletContainer";
+    private static final String JERSEY_INDICATOR_CLASS = "org.glassfish.jersey.servlet.ServletContainer";
 
-	private static final String SERVLET_APPLICATION_CONTEXT_CLASS = "org.springframework.web.context.WebApplicationContext";
+    private static final String SERVLET_APPLICATION_CONTEXT_CLASS = "org.springframework.web.context.WebApplicationContext";
 
-	private static final String REACTIVE_APPLICATION_CONTEXT_CLASS = "org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext";
+    private static final String REACTIVE_APPLICATION_CONTEXT_CLASS = "org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext";
 
-	static WebApplicationType deduceFromClasspath() {
-		if (ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, null) && !ClassUtils.isPresent(WEBMVC_INDICATOR_CLASS, null)
-				&& !ClassUtils.isPresent(JERSEY_INDICATOR_CLASS, null)) {
-			return WebApplicationType.REACTIVE;
-		}
-		for (String className : SERVLET_INDICATOR_CLASSES) {
-			if (!ClassUtils.isPresent(className, null)) {
-				return WebApplicationType.NONE;
-			}
-		}
-		return WebApplicationType.SERVLET;
+    // 从 classpath 上，判断 Web 应用类型。
+    // 可以酱紫的判断的原因是，引入 Spring MVC 时，如果是内嵌的 Web 应用，会引入 Servlet 类。
+    // 2022-01-18：待深入理解ClassUtils.isPresent()
+    static WebApplicationType deduceFromClasspath()
+    {
+	// 存在 Spring Webflux 的类
+	if (ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, null) && !ClassUtils.isPresent(WEBMVC_INDICATOR_CLASS, null)
+		&& !ClassUtils.isPresent(JERSEY_INDICATOR_CLASS, null))
+	{
+	    return WebApplicationType.REACTIVE;
 	}
-
-	static WebApplicationType deduceFromApplicationContext(Class<?> applicationContextClass) {
-		if (isAssignable(SERVLET_APPLICATION_CONTEXT_CLASS, applicationContextClass)) {
-			return WebApplicationType.SERVLET;
-		}
-		if (isAssignable(REACTIVE_APPLICATION_CONTEXT_CLASS, applicationContextClass)) {
-			return WebApplicationType.REACTIVE;
-		}
+	for (String className : SERVLET_INDICATOR_CLASSES)
+	{
+	    // 不存在 Servlet 的类
+	    if (!ClassUtils.isPresent(className, null))
+	    {
 		return WebApplicationType.NONE;
+	    }
 	}
+	return WebApplicationType.SERVLET;
+    }
 
-	private static boolean isAssignable(String target, Class<?> type) {
-		try {
-			return ClassUtils.resolveClassName(target, null).isAssignableFrom(type);
-		}
-		catch (Throwable ex) {
-			return false;
-		}
+    static WebApplicationType deduceFromApplicationContext(Class<?> applicationContextClass)
+    {
+	if (isAssignable(SERVLET_APPLICATION_CONTEXT_CLASS, applicationContextClass))
+	{
+	    return WebApplicationType.SERVLET;
 	}
+	if (isAssignable(REACTIVE_APPLICATION_CONTEXT_CLASS, applicationContextClass))
+	{
+	    return WebApplicationType.REACTIVE;
+	}
+	return WebApplicationType.NONE;
+    }
+
+    private static boolean isAssignable(String target, Class<?> type)
+    {
+	try
+	{
+	    return ClassUtils.resolveClassName(target, null).isAssignableFrom(type);
+	} catch (Throwable ex)
+	{
+	    return false;
+	}
+    }
 
 }
